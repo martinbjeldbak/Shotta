7e1172acaf5cd50728cfe176fa93675a2148f133
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2024-02-03 00:17:04 +1100

    Implement every x second feature

--- main.lua
+++ main.lua
@@ -12,12 +12,25 @@ end
 
 ---@alias triggerId string Key use to define the event that Screenshotter can listen to. Unique.
 
+local function tableMerge(table1, table2)
+  local merged = {}
+
+  for k, v in pairs(table1) do
+    merged[k] = v
+  end
+  for k, v in pairs(table2) do
+    merged[k] = v
+  end
+
+  return merged
+end
+
 ---@class Trigger
----@field eventName string|nil
+---@field eventName string
 ---@field triggerFunc (fun(): nil)|nil
 
 ---@type { [triggerId]: Trigger }
-local Triggers = {
+local BlizzardEventTriggers = {
   login = { eventName = "PLAYER_LOGIN", },
   channelChat = { eventName = "CHAT_MSG_CHANNEL", },
   levelUp = {
@@ -36,15 +49,40 @@ local Triggers = {
   auctionWindowShow = { eventName = "AUCTION_HOUSE_SHOW" },
   groupFormed = { eventName = "GROUP_FORMED" },
   tradeAccepted = { eventName = "TRADE_ACCEPT_UPDATE" },
-  everyXMinute = {
-    triggerFunc = function()
-      C_Timer.After(10, function()
-        TakeScreenshot()
-      end)
+}
+
+local function everyXSecond(seconds, callback)
+  C_Timer.After(seconds, function()
+    local continue = callback()
+    if not continue then
+      return
     end
+
+    everyXSecond(seconds, callback)
+  end)
+end
+
+local CustomTriggers = {
+  everyXMinute = {
+    on = true,
+    registerFunc = function(self)
+      self.on = true
+      ns.PrintToChat(format("Registered event everyXMinute!"))
+      everyXSecond(5, function()
+        if self.on then
+          TakeScreenshot()
+        end
+        return self.on
+      end)
+    end,
+    unregisterFunc = function(self)
+      self.on = false
+    end,
   },
 }
 
+
+local AllTriggers = tableMerge(BlizzardEventTriggers, CustomTriggers)
 
 ---@class Event
 ---@field enabled boolean|nil Whether or not the user has enabled this event
@@ -58,9 +96,10 @@ local DB_DEFAULTS = {
   }
 }
 
+
 local screenshotFrame = CreateFrame("Frame")
 screenshotFrame:SetScript("OnEvent", function(_, event)
-  for _, details in pairs(Triggers) do
+  for _, details in pairs(BlizzardEventTriggers) do
     if event == details.eventName then
       ns.PrintToChat(format("Got event %s, taking screenshot!", event))
 
@@ -77,16 +116,22 @@ end)
 ---@param trigger string
 ---@param enabled boolean
 function screenshotFrame:registerUnregisterEvent(trigger, enabled)
-  local eventName = Triggers[trigger].eventName
+  local event = AllTriggers[trigger]
 
-  if not eventName then
-    return
-  end
+  if event.registerFunc == nil then
+    local eventName = AllTriggers[trigger].eventName
 
-  if enabled then
-    self:RegisterEvent(eventName)
+    if enabled then
+      self:RegisterEvent(eventName)
+    else
+      self:UnregisterEvent(eventName)
+    end
   else
-    self:UnregisterEvent(eventName)
+    if enabled then
+      event:registerFunc()
+    else
+      event:unregisterFunc()
+    end
   end
 end
 
@@ -103,12 +148,15 @@ local function EventHandler(self, event, addOnName)
   ---@type ScreenshotterDatabase
   local db = ns.FetchOrCreateDatabase(DB_DEFAULTS)
 
-  ns.InitializeOptions(self, db, Triggers, screenshotFrame, Screenshotter.ADDON_NAME, Screenshotter.VERSION)
+
+  ns.InitializeOptions(self, db, AllTriggers, screenshotFrame,
+    Screenshotter.ADDON_NAME,
+    Screenshotter.VERSION)
 
   --- Persist DB as SavedVariable since we've been using it as a local
   ScreenshotterDB = db
 
-  for trigger, _ in pairs(Triggers) do
+  for trigger, _ in pairs(BlizzardEventTriggers) do
     local enabled = false
 
     if db.screenshottableEvents[trigger] then
@@ -116,11 +164,24 @@ local function EventHandler(self, event, addOnName)
       enabled = db.screenshottableEvents[trigger].enabled
     end
 
-
     screenshotFrame:registerUnregisterEvent(trigger, enabled)
   end
 
   self:UnregisterEvent(event)
+
+  for trigger, v in pairs(CustomTriggers) do
+    local enabled = false
+
+    if db.screenshottableEvents[trigger] then
+      ---@type boolean enabled should always be there after this if check
+      enabled = db.screenshottableEvents[trigger].enabled
+    end
+
+    if enabled then
+      v:registerFunc()
+    else
+    end
+  end
 
   ns.PrintToChat(Screenshotter.VERSION .. " loaded")
 end
