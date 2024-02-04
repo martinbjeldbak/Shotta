d9723c1384256dab7126e70ed0319e56a629b9d3
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2024-02-04 16:52:55 +1100

    Merge blizzard and non-blizzard triggers together

--- main.lua
+++ main.lua
@@ -15,48 +15,6 @@ local function TakeScreenshot(text)
   Screenshot()
 end
 
----@alias triggerId string Key use to define the event that Screenshotter can listen to. Unique.
-
-local function tableMerge(table1, table2)
-  local merged = {}
-
-  for k, v in pairs(table1) do
-    merged[k] = v
-  end
-  for k, v in pairs(table2) do
-    merged[k] = v
-  end
-
-  return merged
-end
-
----@class Trigger
----@field eventName string
----@field triggerFunc (fun(): nil)|nil
-
----@type { [triggerId]: Trigger }
-local BlizzardEventTriggers = {
-  login = { eventName = "PLAYER_LOGIN", },
-  channelChat = { eventName = "CHAT_MSG_CHANNEL", },
-  levelUp = {
-    eventName = "PLAYER_LEVEL_UP",
-    triggerFunc = function()
-      C_Timer.After(0.5, function()
-        TakeScreenshot()
-      end)
-    end
-  },
-  mailboxOpened = { eventName = "MAIL_SHOW", },
-  readyCheck = { eventName = "READY_CHECK", },
-  zoneChanged = { eventName = "ZONE_CHANGED", },
-  zoneChangedNewArea = { eventName = "ZONE_CHANGED_NEW_AREA", },
-  movementStart = { eventName = "PLAYER_STARTED_MOVING", },
-  auctionWindowShow = { eventName = "AUCTION_HOUSE_SHOW" },
-  groupFormed = { eventName = "GROUP_FORMED" },
-  tradeAccepted = { eventName = "TRADE_ACCEPT_UPDATE" },
-}
-
-
 local function everyXSecond(seconds, callback)
   C_Timer.After(seconds, function()
     local continue = callback()
@@ -72,10 +30,125 @@ local function everyXMinute(minutes, callback)
   everyXSecond(minutes * 60, callback)
 end
 
-local CustomTriggers = {
+
+---@alias triggerId string Key use to define the event that Screenshotter can listen to. Unique.
+---@class Trigger
+---@field eventName string
+---@field triggerFunc (fun(): nil)|nil
+---@type { [triggerId]: Trigger }
+local BlizzardEventTriggers = {
+  login = {
+    eventName = "PLAYER_LOGIN",
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  channelChat = {
+    eventName = "CHAT_MSG_CHANNEL",
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  levelUp = {
+    eventName = "PLAYER_LEVEL_UP",
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+
+    triggerFunc = function()
+      C_Timer.After(0.5, function()
+        TakeScreenshot()
+      end)
+    end
+  },
+  mailboxOpened = {
+    eventName = "MAIL_SHOW",
+
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  readyCheck = {
+    eventName = "READY_CHECK",
+
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  zoneChanged = {
+    eventName = "ZONE_CHANGED",
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  zoneChangedNewArea = {
+    eventName = "ZONE_CHANGED_NEW_AREA",
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  movementStart = {
+    eventName = "PLAYER_STARTED_MOVING",
+
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  auctionWindowShow = {
+    eventName = "AUCTION_HOUSE_SHOW",
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  groupFormed = {
+    eventName = "GROUP_FORMED",
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
+  tradeAccepted = {
+    eventName = "TRADE_ACCEPT_UPDATE",
+
+    register = function(self, frame)
+      frame:RegisterEvent(self.eventName)
+    end,
+    unregister = function(self, frame)
+      frame:UnregisterEvent(self.eventName)
+    end,
+  },
   every5Minutes = {
     on = true,
-    registerFunc = function(self)
+    register = function(self)
       self.on = true
       everyXMinute(5, function()
         if self.on then
@@ -84,7 +157,7 @@ local CustomTriggers = {
         return self.on
       end)
     end,
-    unregisterFunc = function(self)
+    unregister = function(self)
       self.on = false
     end,
   },
@@ -105,9 +178,6 @@ local CustomTriggers = {
   },
 }
 
-
-local AllTriggers = tableMerge(BlizzardEventTriggers, CustomTriggers)
-
 ---@class Event
 ---@field enabled boolean|nil Whether or not the user has enabled this event
 
@@ -125,6 +195,8 @@ local screenshotFrame = CreateFrame("Frame")
 screenshotFrame:SetScript("OnEvent", function(_, event)
   for _, details in pairs(BlizzardEventTriggers) do
     if event == details.eventName then
+      ns.PrintToChat(format("Got event \"%s\", taking screenshot!", event))
+
       if details.triggerFunc == nil then
         TakeScreenshot()
       else
@@ -138,23 +210,29 @@ end)
 ---@param trigger string
 ---@param enabled boolean
 function screenshotFrame:registerUnregisterEvent(trigger, enabled)
-  local event = AllTriggers[trigger]
+  local event = BlizzardEventTriggers[trigger]
 
-  if event.register == nil then
-    local eventName = AllTriggers[trigger].eventName
-
-    if enabled then
-      self:RegisterEvent(eventName)
-    else
-      self:UnregisterEvent(eventName)
-    end
+  if enabled then
+    event:register(self)
   else
-    if enabled then
-      event:register()
-    else
-      event:unregister()
-    end
+    event:unregister(self)
   end
+
+  -- if event.register == nil then
+  --   local eventName = AllTriggers[trigger].eventName
+
+  --   if enabled then
+  --     self:RegisterEvent(eventName)
+  --   else
+  --     self:UnregisterEvent(eventName)
+  --   end
+  -- else
+  --   if enabled then
+  --     event:register()
+  --   else
+  --     event:unregister()
+  --   end
+  -- end
 end
 
 local function EventHandler(self, event, addOnName)
@@ -175,7 +253,7 @@ local function EventHandler(self, event, addOnName)
     version = "dev"
   end
 
-  ns.InitializeOptions(self, db, AllTriggers, screenshotFrame,
+  ns.InitializeOptions(self, db, BlizzardEventTriggers, screenshotFrame,
     Screenshotter.ADDON_NAME, version)
 
   --- Persist DB as SavedVariable since we've been using it as a local
@@ -193,20 +271,6 @@ local function EventHandler(self, event, addOnName)
   end
 
   self:UnregisterEvent(event)
-
-  for trigger, v in pairs(CustomTriggers) do
-    local enabled = false
-
-    if db.screenshottableEvents[trigger] then
-      ---@type boolean enabled should always be there after this if check
-      enabled = db.screenshottableEvents[trigger].enabled
-    end
-
-    if enabled then
-      v:registerFunc()
-    else
-    end
-  end
 
   ns.PrintToChat(version .. " loaded")
 end
