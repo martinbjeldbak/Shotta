8dd12b2ef16d3e9900f221ecb1642dd35c03b1df
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2024-05-05 13:22:49 +1000

    feat: add Chinese translations

---
+++ localization.zhCN.lua
@@ -0,0 +1,32 @@
+local _, ns = ...
+
+-- Translated by https://github.com/EKE00372
+if GetLocale() == "zhCN" then
+	ns.T["checkboxText.channelChat"] = "在频道中发言"
+	ns.T["checkboxText.movementStart"] = "开始移动时（便于测试）"
+	ns.T["checkboxText.levelUp"] = "升级时"
+	ns.T["checkboxText.zoneChangedNewArea"] = "进入新的区域或地区"
+	ns.T["checkboxText.zoneChanged"] = "进入不同的区域或建筑"
+	ns.T["checkboxText.login"] = "登录时"
+	ns.T["checkboxText.readyCheck"] = "准备确认"
+	ns.T["checkboxText.mailboxOpened"] = "打开邮箱"
+	ns.T["checkboxText.auctionWindowShow"] = "拍卖行窗口打开"
+	ns.T["checkboxText.groupFormed"] = "加入或创建小队时"
+	ns.T["checkboxText.tradeAccepted"] = "交易窗口被玩家接受时"
+	ns.T["checkboxText.every5Minutes"] = "每5分钟"
+	ns.T["checkboxText.every10Minutes"] = "每10分钟"
+	ns.T["checkboxText.every30Minutes"] = "每30分钟"
+	ns.T["checkboxText.onDeath"] = "死亡"
+	ns.T["checkboxText.chatAllEmotesWithToken"] = "使用了表情宏命令的表情"
+	ns.T["checkboxText.bossKill"] = "击杀首领时，包括团队"
+	ns.T["checkboxText.encounterEnd"] = "地下城或团队战斗结束时"
+	ns.T["checkboxText.questFinished"] = "完成任务"
+	ns.T["checkboxText.hearthstoneBound"] = "绑定炉石"
+	ns.T["checkboxText.lootItemRollWin"] = "赢得拾取骰子"
+	ns.T["checkboxText.profile.hideMiniMap"] = "隐藏小地图图标"
+	ns.T["checkboxText.achievementEarned"] = "获得成就"
+	ns.T["events"] = "事件"
+	ns.T["settings"] = "设置"
+	ns.T["about"] = "关于"
+	ns.T["saveLocationHelpText"] = "截图将保存到操作系统的默认位置"
+end
---
+++ localization.zhTW.lua
@@ -0,0 +1,32 @@
+local _, ns = ...
+
+-- Translated by https://github.com/EKE00372
+if GetLocale() == "zhTW" then
+	ns.T["checkboxText.channelChat"] = "在頻道中發言"
+	ns.T["checkboxText.movementStart"] = "開始移動時（便於測試）"
+	ns.T["checkboxText.levelUp"] = "升級"
+	ns.T["checkboxText.zoneChangedNewArea"] = "進入新區域或地區"
+	ns.T["checkboxText.zoneChanged"] = "進入不同的區域或建築"
+	ns.T["checkboxText.login"] = "登入時"
+	ns.T["checkboxText.readyCheck"] = "準備確認"
+	ns.T["checkboxText.mailboxOpened"] = "打開信箱"
+	ns.T["checkboxText.auctionWindowShow"] = "打開拍賣行"
+	ns.T["checkboxText.groupFormed"] = "加入或創建小隊"
+	ns.T["checkboxText.tradeAccepted"] = "玩家確認交易視窗時"
+	ns.T["checkboxText.every5Minutes"] = "每5分鐘"
+	ns.T["checkboxText.every10Minutes"] = "每10分鐘"
+	ns.T["checkboxText.every30Minutes"] = "每30分鐘"
+	ns.T["checkboxText.onDeath"] = "死亡"
+	ns.T["checkboxText.chatAllEmotesWithToken"] = "使用了表情的表情指令"
+	ns.T["checkboxText.bossKill"] = "擊殺首領時，包括團隊"
+	ns.T["checkboxText.encounterEnd"] = "地城或團隊戰鬥結束"
+	ns.T["checkboxText.questFinished"] = "完成任務"
+	ns.T["checkboxText.hearthstoneBound"] = "綁定爐石"
+	ns.T["checkboxText.lootItemRollWin"] = "贏得拾取骰子"
+	ns.T["checkboxText.profile.hideMiniMap"] = "隱藏小地圖圖示"
+	ns.T["checkboxText.achievementEarned"] = "獲得成就"
+	ns.T["events"] = "事件"
+	ns.T["settings"] = "設定"
+	ns.T["about"] = "關於"
+	ns.T["saveLocationHelpText"] = "截圖儲存在你的作業系統中預設的路徑"
+end
